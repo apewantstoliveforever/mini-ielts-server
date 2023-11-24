9810eb5c9728570426d681403c3482b7c4a276be
Jake Hu <hotranphihung@gmail.com>
2023-11-24 20:24:49 +0700

    s

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,9 +1,10 @@
 //authorization.js
 
-const adminArray = ["foreveralone1608@gmail.com, admin@gmail.com"]
+const adminArray = ["foreveralone1608@gmail.com", "admin@gmail.com"]
 
 const AdminRole = (req, res, next) => {
     const email = req.user.email
+    console.log(email)
     if (adminArray.includes(email)) {
         next()
     } else {
