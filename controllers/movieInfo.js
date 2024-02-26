1436e6a72e4b06fe4534b9ab5c432b4a44e19afc
Jake Hu <hotranphihung@gmail.com>
2024-02-26 09:44:33 +0700

    s

--- controllers/movieInfo.js
+++ controllers/movieInfo.js
@@ -15,7 +15,19 @@ const db = mysql.createConnection({
 const getMovieInfo = (req, res) => {
     //parameters are sent in the request object
     const movieId = req.params.movieId;
-    console.log(movieId);
+    //get movie info from the database
+    db.query('SELECT * FROM movies WHERE id = ?', [movieId], (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            if (results.length > 0) {
+                res.status(200).send(results[0]);
+            } else {
+                res.status(404).send('Movie not found');
+            }
+        }
+    });
 };
 
 module.exports = {
