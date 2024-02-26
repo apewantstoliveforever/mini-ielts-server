950f448b85964e5551859653a1f25052685f5a72
Jake Hu <hotranphihung@gmail.com>
2024-02-26 13:45:22 +0700

    s

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -10,8 +10,8 @@ const movieInfo = require('../controllers/movieInfo');
 // Define a route
 
 router.get('/movieInfo/:movieId', movieInfo.getMovieInfo);
-router.get('/addMovie', addMovie.addMovie);
-router.get('/addListMovies', addMovie.addListMovies);
+// router.get('/addMovie', addMovie.addMovie);
+// router.get('/addListMovies', addMovie.addListMovies);
 router.get('/page/:page', movieInfo.getALlMoviesInPage);
 router.get('/pages', movieInfo.getNumberOfPages);
 router.get('/genre/:genre', movieInfo.getMoviesByGenre);
